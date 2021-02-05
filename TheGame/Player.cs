b7383d6d70d02a992cccb8e00600e977d38a8866
Ingrid Pettersson <67603250+IngridPettersson@users.noreply.github.com>
2021-02-05 16:55:30 +0100

    Add changes

--- TheGame/Player.cs
+++ TheGame/Player.cs
@@ -2,7 +2,7 @@
 {
     internal class Player : Character
     {
-        public Player() : base("Madame E Petit", 20, 10)
+        public Player() : base("Madame E Petit", 100, 10)
         {
             X = 0;
             Y = 0;
